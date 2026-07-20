95268d38710c71ede49b287160a94f2837312b8d
HuyH2026 <300537542+HuyH2026@users.noreply.github.com>
2026-07-20 11:49:31 -0700

    feat: rebuild Home screen as fluid content surface

--- src/features/home/HomeScreen.tsx
+++ src/features/home/HomeScreen.tsx
@@ -1,3 +1,3 @@
 export function HomeScreen() {
-  return <div data-testid="screen-home" className="h-full rounded-[26px] bg-white" />
+  return <div data-testid="screen-home" className="h-full w-full rounded-[26px] bg-white" />
 }
